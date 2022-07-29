debe62a1ec06e57fb479edec47c18d9d4e3ceecf
Qais <ww258148@gmail.com>
2022-07-30 00:41:38 +0300

    Many changes have been added.

--- change_theme_dialog.py
+++ change_theme_dialog.py
@@ -1,23 +1,24 @@
 #-*- coding: utf-8 -*-
 # import project libraries.
 import wx
+from ChLanguages import *
 
 colourList = ["Aquamarine", "Black", "Blue", "Blue Violet", "Brown", "Cadet Blue", "Coral", "Cornflower Blue", "Cyan", "Dark Grey", "Dark Green", "Brown"]
 
 class ChangeTheme(wx.Dialog):
 
 	def __init__(self):
-		super().__init__(None, title = "change theme", size=(200, 200))
+		super().__init__(None, title = _("Change theme"), size=(200, 200))
 		self.Center()
 		self.Maximize(False)
 		Panel = wx.Panel(self)
-		wx.StaticText(Panel, -1, "Choose theme", pos=(20,20), size=(80, 30))
+		wx.StaticText(Panel, -1, _("Choose theme"), pos=(20,20), size=(80, 30))
 		self.ChangeTheme = wx.ComboBox(Panel, -1, pos=(20, 50), size=(140, 40), style=wx.CB_READONLY+wx.CB_SORT)
 		self.ChangeTheme.SetItems(colourList)
 		# Create Buttons
-		self.Change = wx.Button(Panel, -1, "change", pos=(20,100), size=(60,30))
+		self.Change = wx.Button(Panel, -1, _("&Change"), pos=(20,100), size=(60,30))
 		self.Change.SetDefault()
-		self.Close = wx.Button(Panel, wx.ID_CANCEL, "Cancel", pos=(90,100), size=(60,30))
+		self.Close = wx.Button(Panel, wx.ID_CANCEL, _("&Cancel"), pos=(90,100), size=(60,30))
 
 		self.Change.Bind(wx.EVT_BUTTON, self.OnChangeTheme)
 
--- view_article_window.py
+++ view_article_window.py
@@ -3,15 +3,20 @@
 import wx
 import nlpia2_wikipedia as wikipedia
 import pyperclip
-from change_theme_dialog import ChangeTheme
+import accessible_output2.outputs.auto
 import os
+import re
+from change_theme_dialog import ChangeTheme
+from headings_list_dialog import HeadingsListDialog
+from settings import Settings
+from ChLanguages import *
 
 colourList = ["Aquamarine", "Black", "Blue", "Blue Violet", "Brown", "Cadet Blue", "Coral", "Cornflower Blue", "Cyan", "Dark Grey", "Dark Green", "Brown"]
 
 #View Article window
 class ViewArticleWindow(wx.Frame):
 	def __init__(self, parent, GetValues, handle):
-		super().__init__(parent, title="View Article", size=(560, 600))
+		super().__init__(parent, title=_("View article"), size=(560, 600))
 		self.Center()
 		self.EnableMaximizeButton(False)
 		self.GetValues = GetValues
@@ -19,51 +24,68 @@ class ViewArticleWindow(wx.Frame):
 		self.url = ""
 		self.title = ""
 		self.handle = handle
+		self.o = accessible_output2.outputs.auto.Auto()
+		self.rand_id = wx.NewIdRef(count=1)
+		self.CurrentSettings = Settings().ReadSettings()
 
 
 		Panel = wx.Panel(self)
 		# Create Menus.
 		menubar = wx.MenuBar()
 		actions = wx.Menu()
-		self.CopyArticleItem = actions.Append(-1, "Copy article\tctrl+shift+c")
-		self.CopyArticleLinkItem = actions.Append(-1, "Copy article link\tctrl+alt+c")
-		self.SaveArticleItem = actions.Append(-1, "save article\tctrl+s")
-		self.ChangeThemeItem = actions.Append(-1, "change Theme\tctrl+T")
-		self.CloseArticleItem = actions.Append(-1, "close article window\tctrl+w")
-		self.CloseProgramItem = actions.Append(-1, "Close the program\tctrl+F4")
-		menubar.Append(actions, "Actions")
+		self.CopyArticleItem = actions.Append(-1, _("Copy article\tctrl+shift+c"))
+		self.CopyArticleItem.Enable(enable=False)
+		self.CopyArticleLinkItem = actions.Append(-1, _("Copy article link\tctrl+alt+c"))
+		self.CopyArticleLinkItem.Enable(enable=False)
+		self.SaveArticleItem = actions.Append(-1, _("Save article\tctrl+s"))
+		self.SaveArticleItem.Enable(enable=False)
+		self.GoToHeading = actions.Append(-1, _("Go to a &heading \tCtrl+h"))
+		self.GoToHeading.Enable(enable=False)
+		self.ChangeThemeItem = actions.Append(-1, _("Change theme\tctrl+T"))
+		self.CloseArticleItem = actions.Append(-1, _("Close article window\tctrl+w"))
+		self.CloseProgramItem = actions.Append(-1, _("Close the program\tctrl+F4"))
+		menubar.Append(actions, _("Actions"))
 		self.SetMenuBar(menubar)
 
 		self.hotKeys = wx.AcceleratorTable([
 			(wx.ACCEL_CTRL+wx.ACCEL_SHIFT, ord("C"), self.CopyArticleItem.GetId()),
 			(wx.ACCEL_CTRL+wx.ACCEL_ALT, ord("C"), self.CopyArticleLinkItem.GetId()),
 			(wx.ACCEL_CTRL, ord("S"), self.SaveArticleItem.GetId()),
+(wx.ACCEL_CTRL, ord("H"), self.GoToHeading.GetId()),
 			(wx.ACCEL_CTRL, ord("T"), self.ChangeThemeItem.GetId()),
 			(wx.ACCEL_CTRL, ord("W"), self.CloseArticleItem.GetId()),
 			(wx.ACCEL_CTRL,wx.WXK_F4, self.CloseProgramItem.GetId()),
+			(0, wx.WXK_ESCAPE, self.rand_id),
 		])
 		Panel.SetAcceleratorTable(self.hotKeys)
 
 		# Create RichEdit to View Article Content
-		self.ArticleTitle = wx.StaticText(Panel, -1, "please wait:", pos=(10,10), size=(380,30))
+		self.ArticleTitle = wx.StaticText(Panel, -1, _("Please wait."), pos=(10,10), size=(380,30))
 		self.ViewArticle = wx.TextCtrl(Panel, -1, pos=(30,40), size=(480,420), style=wx.TE_RICH2+wx.TE_MULTILINE+wx.TE_READONLY)
 
 
 		# Create Buttons
-		self.CopyArticle = wx.Button(Panel, -1, "Copy article\t(ctrl+shift+c)", pos=(10,500), size=(120,30))
-		self.SaveArticle = wx.Button(Panel, -1, "save article\t(ctrl+s)", pos=(140,500), size=(120,30))
+		self.CopyArticle = wx.Button(Panel, -1, _("Copy article\t(ctrl+shift+c)"), pos=(10,500), size=(120,30))
+		self.CopyArticle.Enable(enable=False)
+		self.SaveArticle = wx.Button(Panel, -1, _("Save article\t(ctrl+s)"), pos=(140,500), size=(120,30))
+		self.SaveArticle.Enable(enable=False)
 		self.SaveArticle.SetDefault()
-		self.CopyArticleLink = wx.Button(Panel, -1, "Copy article link\t(ctrl+alt+c)", pos=(270,500), size=(120,30))
-		self.CloseArticle = wx.Button(Panel, -1, "Close Article\t(ctrl+w)", pos=(400,500), size=(120,30))
+		self.CopyArticleLink = wx.Button(Panel, -1, _("Copy article link\t(ctrl+alt+c)"), pos=(270,500), size=(120,30))
+		self.CopyArticleLink.Enable(enable=False)
+		self.CloseArticle = wx.Button(Panel, -1, _("Close article\t(ctrl+w)"), pos=(400,500), size=(120,30))
+
 
 		# Show Article window
 		self.Show()
+
+
 
 		# events for buttons
 		self.CopyArticle.Bind(wx.EVT_BUTTON, self.OnCopyArticle)
 		self.SaveArticle.Bind(wx.EVT_BUTTON, self.OnSaveArticle)
 		self.CopyArticleLink.Bind(wx.EVT_BUTTON, self.OnCopyArticleLink)
 		self.CloseArticle.Bind(wx.EVT_BUTTON, self.OnCloseArticle)
+		self.Bind(wx.EVT_CLOSE, self.OnCloseArticle)
 
 		# events for Menus
 		self.Bind(wx.EVT_MENU, self.OnCopyArticle, self.CopyArticleItem) 
@@ -72,38 +94,61 @@ class ViewArticleWindow(wx.Frame):
 		self.Bind(wx.EVT_MENU, self.OnCloseArticle, self.CloseArticleItem) 
 		self.Bind(wx.EVT_MENU, self.OnChangeTheme, self.ChangeThemeItem) 
 		self.Bind(wx.EVT_MENU, self.OnCloseProgram, self.CloseProgramItem) 
+		self.Bind(wx.EVT_MENU, self.OnGoToheading, self.GoToHeading)
+		self.Bind(wx.EVT_MENU, self.OnEscape, id=self.rand_id)
 
 	def OpenThread(self):
+
 		try:
 			self.Content = wikipedia.page(self.GetValues).content
+			self.o.speak(_("Loading article:"), interrupt=True)
 		except wikipedia.exceptions.DisambiguationError as e:
-			mgb = wx.MessageDialog(self, """This article is no longer available.
+			mgb = wx.MessageDialog(self, _("""This article is no longer available.
 do you want to show similar results for this  article?
-""", "warning", style=wx.YES_NO+wx.YES_DEFAULT+wx.ICON_QUESTION)
+"""), _("Warning"), style=wx.YES_NO+wx.YES_DEFAULT+wx.ICON_QUESTION+wx.ICON_WARNING)
 			if mgb.ShowModal() == wx.ID_YES:
 				self.Destroy()
-#				wx.Window.Close(self.handle)
 				self.handle.ListResults.SetItems(e.options)
 			else:
 				self.Destroy()
+			self.handle.NumberArticle -=1
 			return None
-		self.url = wikipedia.page(self.GetValues).url
+		except :
+			wx.MessageBox("There is no internet connection.", "Connection error", style=wx.ICON_ERROR)
+			self.Destroy()
+			self.handle.NumberArticle -=1
+			return None
+
 		self.title = wikipedia.page(self.GetValues).title
 		self.ViewArticle.Value = self.Content
-		self.SetTitle(f"View {self.title}")
+		self.SetTitle(_("View {}").format(self.title))
 		self.ArticleTitle.SetLabel(self.title)
+		self.GoToHeading.Enable(enable=True)
+		self.CopyArticleItem.Enable(enable=True)
+		self.CopyArticleLinkItem.Enable(enable=True)
+		self.SaveArticleItem.Enable(enable=True)
+		self.CopyArticle.Enable(enable=True)
+		self.SaveArticle.Enable(enable=True)
+		self.CopyArticleLink.Enable(enable=True)
+		self.url = wikipedia.page(self.GetValues).url
+		self.o.speak(_("Article loaded"), interrupt=True)
 
 	# Copy Article Content
 	def OnCopyArticle(self, event):
 		pyperclip.copy(self.Content)
+		self.o.speak(_("Article copied."), interrupt=False)
+
 
 	# Copy Article Link
 	def OnCopyArticleLink(self, event):
 		pyperclip.copy(self.url)
+		self.o.speak(_("Article link copied."), interrupt=False)
 
-	# Save Article On a New File.
+
+
+		# Save Article On a New File.
 	def OnSaveArticle(self, event):
-		SaveFile = wx.FileDialog(self, F"Save {self.title}:", "self.FilePath", F"{self.title}", style=wx.FD_SAVE+wx.FD_OVERWRITE_PROMPT)
+		SaveFile = wx.FileDialog(self, _("Save {}:").format(self.title), "self.FilePath", F"{self.title}", style=wx.FD_SAVE+wx.FD_OVERWRITE_PROMPT)
 		SaveFile.Wildcard = "Text files (.txt)|*.txt"
 		SaveFileResult = SaveFile.ShowModal()
 		if SaveFileResult == wx.ID_OK:
@@ -124,9 +169,40 @@ do you want to show similar results for this  article?
 
 	# Close Article Window
 	def OnCloseArticle(self, event):
+		self.handle.NumberArticle -= 1
 		self.Destroy()
 
 
 		# Close Program 
 	def OnCloseProgram(self, event):
-		wx.Exit()
+
+		state = self.CurrentSettings["CloseMessage"]
+
+		if self.handle.NumberArticle == 1:
+			ArticleCounte = _("There is 1 open article.")
+		elif self.handle.NumberArticle > 1:
+			ArticleCounte = _("There are {} open articles.").format(self.handle.NumberArticle)
+		if (self.handle.NumberArticle >= 1) and (state == "True"):
+			ConfirmClosProgram = wx.MessageDialog(self, _("""{}
+Do you want to close the program anyway?""").format(ArticleCounte), "Confirm", style=wx.YES_NO+wx.YES_DEFAULT+wx.ICON_WARNING+wx.ICON_QUESTION)
+			if ConfirmClosProgram.ShowModal() == wx.ID_YES:
+								wx.Exit()
+			else:
+				return
+		else:
+			wx.Exit()
+
+	def OnGoToheading(self, event):
+		position = HeadingsListDialog(self, self.Content).ShowModal()
+		self.ViewArticle.SetInsertionPoint(position)
+		self.ViewArticle.SetFocus()
+
+	def OnEscape(self, event):
+
+		state = self.CurrentSettings["ActivEscape"]
+
+		if state == "True":
+			self.handle.NumberArticle -= 1
+			self.Destroy()
+		else:
+			pass
