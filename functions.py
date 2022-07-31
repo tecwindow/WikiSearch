98009de94db5866a803f0bb4c38c13cb23c8c38d
AhmedBakr <44323153+MesterPerfect@users.noreply.github.com>
2022-07-31 15:59:58 +0200

    Add comments to language control function

--- functions.py
+++ functions.py
@@ -1,6 +1,13 @@
 ﻿import gettext
 
+# Create a function to add translations to the program
+# This function is used to identify the program with translations added to it by contributors
+
 def SetLanguage(CurrentSettings):
+
+# This dictionary is used to define program on language folder and on name of language within program settings
+# The dictionary key indicates the name of the language within the program settings. For example: "English"
+# The dictionary value indicates the folder that contains the language files. Example: "en"
 
 	language = {
 	"english": "en", 
