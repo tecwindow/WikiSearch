53df39757c220c3fbbb251873c068991a27c99c6
AhmedBakr <44323153+MesterPerfect@users.noreply.github.com>
2022-08-09 16:04:39 +0200

    Increase size settings Window

--- settings.py
+++ settings.py
@@ -74,7 +74,7 @@ _ = SetLanguage(Settings().ReadSettings())
 # Create Settings Dialog
 class SettingsDialog(wx.Dialog):
 	def __init__(self):
-		super().__init__(None, title=_("Program Settings"), size=(400, 360))
+		super().__init__(None, title=_("Program Settings"), size=(400, 500))
 		#make window in center.
 		self.Center()
 		#make window Minimum size.
@@ -87,41 +87,41 @@ class SettingsDialog(wx.Dialog):
 
 
 		# Creating ComboBox for languages
-		wx.StaticText(Panel, -1, _("Choose language:"), pos=(15,20), size=(380, 30))
-		self.ProgramLanguage = wx.ComboBox(Panel, -1, choices=['Arabic', 'English', 'Español', 'Français',], pos=(15, 50), size=(120, 40), style=wx.CB_READONLY+wx.CB_SORT)
+		wx.StaticText(Panel, -1, _("Choose language:"), pos=(15,120), size=(380, 30))
+		self.ProgramLanguage = wx.ComboBox(Panel, -1, choices=['Arabic', 'English', 'Español', 'Français',], pos=(15, 150), size=(120, 40), style=wx.CB_READONLY+wx.CB_SORT)
 		self.ProgramLanguage.Value = self.CurrentSettings["language"]
 
 		#creating RadioBox to chews Favorite  view.
-		self.viewer = wx.RadioBox(Panel, -1, label=_("Choose your prefered view:"), choices=[_("Normal View"), _("Web View(Not recommended)")], pos=(10 , 10), size=(380,50)) 
+		self.viewer = wx.RadioBox(Panel, -1, label=_("Choose your prefered view:"), choices=[_("Normal View"), _("Web View(Not recommended)")], pos=(10 , 20), size=(350,80), style=wx.RB_SINGLE)
 		self.viewer.Selection = int(self.CurrentSettings["wepviewer"])
 
 		# Creating SpinCtrl for Results Number
-		wx.StaticText(Panel, -1, _("Select the number of results:"), pos=(140,20), size=(380, 30))
-		self.NumberResults = wx.SpinCtrl(Panel, -1, "20", min=1, max=100, style=wx.SP_ARROW_KEYS, pos=(160, 50), size=(50, 20))
+		wx.StaticText(Panel, -1, _("Select the number of results:"), pos=(140,120), size=(380, 30))
+		self.NumberResults = wx.SpinCtrl(Panel, -1, "20", min=1, max=100, style=wx.SP_ARROW_KEYS, pos=(160, 150), size=(50, 20))
 		self.NumberResults.Value = self.CurrentSettings["results number"]
 
 		# Creating SpinCtrl for random article Number
-		wx.StaticText(Panel, -1, _("Select the number of random articles:"), pos=(10,175), size=(380, 30))
-		self.random_articles_number = wx.SpinCtrl(Panel, -1, "20", min=1, max=100, style=wx.SP_ARROW_KEYS, pos=(30, 210), size=(50, 20))
+		wx.StaticText(Panel, -1, _("Select the number of random articles:"), pos=(10,260), size=(380, 30))
+		self.random_articles_number = wx.SpinCtrl(Panel, -1, "20", min=1, max=100, style=wx.SP_ARROW_KEYS, pos=(30, 290), size=(50, 20))
 		self.random_articles_number.Value = self.CurrentSettings["random articles number"]
 
 		# Creating Check Boxes
-		self.VerificationMsg = wx.CheckBox(Panel, -1, label=_("Show Close message when at least an article is open"), pos=(10, 80), size=(380, 30))
+		self.VerificationMsg = wx.CheckBox(Panel, -1, label=_("Show Close message when at least an article is open"), pos=(10, 180), size=(380, 30))
 		if self.CurrentSettings["close message"] == "True":
 			self.VerificationMsg.Value = True
 
-		self.AutoUpdate = wx.CheckBox(Panel, -1, label=_("Check for updates automatically"), pos=(10, 110), size=(380, 30))
+		self.AutoUpdate = wx.CheckBox(Panel, -1, label=_("Check for updates automatically"), pos=(10, 205), size=(380, 30))
 		if self.CurrentSettings["auto update"] == "True":
 			self.AutoUpdate.Value = True
 
-		self.CloseArticleWithScape = wx.CheckBox(Panel, -1, label=_("Close the article via the Escape key"), pos=(10, 140), size=(380, 30))
+		self.CloseArticleWithScape = wx.CheckBox(Panel, -1, label=_("Close the article via the Escape key"), pos=(10, 230), size=(380, 30))
 		if self.CurrentSettings["activ escape"] == "True":
 			self.CloseArticleWithScape.Value = True
 
 		# Create Buttons
-		self.SaveSettings = wx.Button(Panel, -1, _("&Save changes"), pos=(10,255), size=(100,30))
+		self.SaveSettings = wx.Button(Panel, -1, _("&Save changes"), pos=(10,400), size=(100,30))
 		self.SaveSettings.SetDefault()
-		self.GoBack = wx.Button(Panel, wx.ID_CANCEL, _("&Cancel"), pos=(120,255), size=(80,30))
+		self.GoBack = wx.Button(Panel, wx.ID_CANCEL, _("&Cancel"), pos=(120,400), size=(80,30))
 
 		self.hotKeys = wx.AcceleratorTable([
 			(wx.ACCEL_ALT, ord("S"), self.SaveSettings.GetId()),
