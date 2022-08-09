abb0d5ffde23352694daeee3b190467d47b0a353
MahmoudAtefFarook <109464635+MahmoudAtefFarook@users.noreply.github.com>
2022-08-10 01:04:08 +0200

    Update version number.

--- WikiSearch.py
+++ WikiSearch.py
@@ -36,7 +36,7 @@ for w in data:
 	code.append(w["code"])
 
 # information of program
-CurrntVersion = 1.1
+CurrntVersion = 1.2
 ProgramName = "WikiSearch"
 ProgramDescription = _("With this program, you can search or browse any Wikipedia article. site: https://github.com/tecwindow/WikiSearch")
 CurrentSettings = Settings().ReadSettings()
